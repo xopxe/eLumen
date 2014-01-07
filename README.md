d1e3aa86d9d0ac446ef112b9738b938adffb06b1
Jorge <xxopxe@gmail.com>
2014-01-07 14:33:29 -0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 eLumen
 ======
 
-Coroutine based scheduler for eLua
+Coroutine based scheduler for eLua.
+
+This is basically the [Lumen](https://github.com/xopxe/Lumen) scheduler after being pruned from the less used stuff. There is space for more prunning, tough.
+
+The main difference is that unlike Lumen, were signals are a event plus a vararg of parameters, in eLumen signals are a event plus a single parameter (you can send a table if you wish, of course).
+
+This is tested on a MBED board. To port to another board, the only change neede is in the get_time() function. You must provide a method for reading the time (for example, on MBED it is doing  _return tmr.read(0)_).
+
+There is a test.lua script (also for MBED). It will blink led1 at 1Hz, and togle led2 and led3 with the keys "q" and "a" respectivelly. It will aslo print memory usage, at the script starting time, after loading the scheduler, after setting up the tasks, and then while running once a second.
+
+
+TODO
+----
+
+* Think what more functionality to cull, or restore.
+* Update inline docs.
+* Look into idle() function, perhaps do semething smarter than busy waiting.
+
+
---
+++ test.lua
@@ -0,0 +1,47 @@
+print ('starting', collectgarbage('count') )
+
+package.path = package.path .. ";;;/semi/?.lua"
+local sched = require 'sched'
+
+print ('sched loaded', collectgarbage('count') )
+
+local led1 = mbed.pio.LED1
+local led2 = mbed.pio.LED2
+local led3 = mbed.pio.LED3
+pio.pin.setdir(pio.OUTPUT, led1)
+pio.pin.setdir(pio.OUTPUT, led2)
+pio.pin.setdir(pio.OUTPUT, led3)
+
+-- this task will toggle led1 at 2Hz
+sched.run(function()
+  while true do
+    pio.pin.sethigh(led1)
+    sched.sleep(500000) -- delay is in usecs
+    pio.pin.setlow(led1)
+    sched.sleep(500000) -- delay is in usecs
+    print ('running', collectgarbage('count') )
+  end
+end)
+
+-- this task will emit a signal with a chars arrived from terminal
+sched.run(function()
+  while true do
+    local char = term.getchar(term.NOWAIT)
+    if char > -1 then
+      sched.signal(string.char(char))
+    end
+    sched.wait()
+  end
+end)
+
+-- when catching a signal with specific chars, toggle some leds.
+sched.sigrun({'q', 'Q'}, function()
+    pio.pin.setval( 1-pio.pin.getval(led2), led2)
+end)
+sched.sigrun({'w', 'W'}, function()
+    pio.pin.setval( 1-pio.pin.getval(led3), led3)
+end)
+
+print ('ready to run', collectgarbage('count') )
+
+sched.loop()
